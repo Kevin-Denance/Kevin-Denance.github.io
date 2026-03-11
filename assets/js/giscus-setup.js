b53d860364b978b97153f5672fda9df8a3e38da3
Kevin-Denance <105646649+Kevin-Denance@users.noreply.github.com>
2026-03-11 22:18:56 +0000

    Deploying to gh-pages from @ Kevin-Denance/kevin-denance.github.io@4f5c17a429c4bb5c3e575253c39f6f5dfef17950 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "Kevin-Denance/kevin-denance.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
